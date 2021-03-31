4e354a6fe6ab4ae529f0ad0a137f9181ebe01485
Titouan Pellerin <46569260+titouan-pellerin@users.noreply.github.com>
2021-03-31 22:25:45 +0200

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,6 +1,6 @@
-let mapbox = 'pk.eyJ1IjoidGl0b3VhYW4iLCJhIjoiY2ttbTZnMTYzMGQwbDJvbHcwd3l4MDdzZyJ9.9ckoy9LTQ3nzMhl_HuwMXQ';
-let spotifyId = 'f844d414d7da4b7897082fa56d8d9542';
-let spotifySecret = '3df110b501d3494f994ed343c6b3d2d6';
+let mapbox = '';
+let spotifyId = '';
+let spotifySecret = '';
 
 module.exports = {
     mapbox, spotifyId, spotifySecret
